339bbcce979a8b03378802f4351e7f9a0f05f191
Mychal <Mychal_Clark@insideranken.org>
2020-01-13 14:27:08 -0600

    HOT3 Completed

---
+++ Hands On Test/CH3/EX2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type A Number");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Type A Number");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Type A Number");
+           double num3 =Convert.ToDouble(Console.ReadLine());
+
+            double avg = (num3 + num2 + num1) / 3;
+
+            Console.WriteLine($"Your average is {avg}");
+        }
+    }
+}
---
+++ Hands On Test/CH3/EX3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name:  "); string name =Console.ReadLine();
+            Console.Write("Street Address:  "); string addy = Console.ReadLine();
+            Console.Write("City:  "); string city = Console.ReadLine();
+            Console.Write("State:  "); string state = Console.ReadLine();
+            Console.Write("Zip:  "); string zip =Console.ReadLine();
+            Console.Write("How Many T-Shirts did you want to order?  "); int shirtOrder = Convert.ToInt32(Console.ReadLine());
+
+            Convert.ToDouble(shirtOrder);
+            double totalPrice = shirtOrder * 14.99;
+            double salesTax = totalPrice * 0.08;
+            double amountDue = salesTax + totalPrice;
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+
+            Console.WriteLine("Receipt for:");
+            Console.WriteLine($"{name}");
+            Console.WriteLine($"{addy}");
+            Console.WriteLine($"{city}, {state} {zip}");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            Console.WriteLine($"{shirtOrder} T-Shirts ordered @ $14.99 each");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            Console.WriteLine($"Total:        {totalPrice:C}");
+            Console.WriteLine($"Tax:          {salesTax:C}");
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine($"Due:          {amountDue:C}");
+
+        }
+    }
+}
